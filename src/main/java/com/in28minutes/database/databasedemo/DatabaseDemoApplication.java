59852f65e771e5a60f672dedf61bdc02de6db555
Roman Vilensky <rvilensky@paciolan.com>
2019-04-12 13:44:24 -0700

    first commit

---
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -0,0 +1,31 @@
+package com.in28minutes.database.databasedemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDao;
+
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		logger.info("All users -> {}", dao.findAll());
+
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO person
+	(ID, NAME, LOCATION, BIRTH_DATE)
+VALUES
+	(10001, 'Roman', 'Hyderabad', sysdate());
+	
+INSERT INTO person
+	(ID, NAME, LOCATION, BIRTH_DATE)
+VALUES
+	(10002, 'Elazar', 'New York', sysdate());
+	
+INSERT INTO person
+	(ID, NAME, LOCATION, BIRTH_DATE)
+VALUES
+	(10003, 'Elmira', 'LA', sysdate());
